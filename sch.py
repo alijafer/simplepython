4d19f0986bf817fea4152abb57e48b1796e96935
Ali Jafer <alijaferalamer@gmail.com>
2022-02-18 20:07:40 +0300

    add api request

--- sch.py
+++ sch.py
@@ -4,21 +4,34 @@ import os
 import schedule
 import time
 import logging
+import requests
 
 
+url ="http://127.0.0.1:8000/"
 
 file ="c:/Users/ali29/python code/temp/"+ str(datetime.now().strftime('%d-%m-%Y %Hh%Mm'))+ ".log"
 fmt='[%(levelname)s] %(asctime)s %(message)s'
 # Create and configure logger
 logging.basicConfig(filename=file,
                     format=fmt,
-                    filemode='a')
+                    filemode='a', encoding='UTF-8')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def job():
+  try:
+
+    x = requests.get(url)
+  
+    print(x.json())
+    logger.info(x.json())
+  except Exception as e:
+    # print(f"connot accsess{e}")
+    # logger.error(e)
+    logger.exception(e)
   num = secrets.randbelow(255)
   logger.info(f"number {num}")
+
 
 
 schedule.every(10).seconds.do(job)
